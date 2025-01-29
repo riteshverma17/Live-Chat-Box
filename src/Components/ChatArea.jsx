6f15d1a04ce7ef729995cc93ee18dc68213c3aaf
riteshverma17 <riteshjii.04@gmail.com>
2025-01-29 11:54:42 +0530

    -

--- src/Components/ChatArea.jsx
+++ src/Components/ChatArea.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./myStyles.css";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
 
-function ChatArea() {
+function ChatArea({ props }) {
   return (
-    <div className='chatArea-container'>
-      <div className="chatArea-header">ChatArea-Header</div>
+    <div className="chatArea-container">
+      <div className="chatArea-header">
+        <div className="combo">
+          <div className="con-icon">T</div>
+          <div className="header-text">
+            <div className="con-title">Test1</div>
+            <div className="con-timeStamp">today</div>
+          </div>
+        </div>
+
+        <div className="delete-icon">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      </div>
       <div className="message-container">ChatArea-Message</div>
-      <div className="text-input-area">ChatArea-Header</div>
+      <div className="text-input-area">
+        <input type="text" placeholder="Type a message..." className="inputTEXT-box" />
+        <IconButton>
+          <SendIcon />
+        </IconButton>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatArea
+export default ChatArea;
--- src/Components/ConversationItem.jsx
+++ src/Components/ConversationItem.jsx
@@ -4,7 +4,7 @@ import "./myStyles.css";
 function ConversationItem({props}) {
   return (
     <div className='conversation-container'>
-      <p className='com-icon'>{props.name[0]}</p>
+      <p className='con-icon'>{props.name[0]}</p>
       <p className='con-title'>{props.name}</p>
       <p className='con-lastMessage'>{props.lastMessage}</p>
       <p className='con-timeStamp'>{props.timeStamp}</p>
